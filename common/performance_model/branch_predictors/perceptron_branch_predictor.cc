f22be03e1c706ff9ec90ce7162e3fd5a549f34e5
fanyx <fanyx@shanghaitech.edu.cn>
2020-12-09 16:26:53 +0800

    add some comments

--- common/performance_model/branch_predictors/perceptron_branch_predictor.cc
+++ common/performance_model/branch_predictors/perceptron_branch_predictor.cc
@@ -9,6 +9,7 @@ void print(std::string s) {
     std::cout << s  << std::endl;
 }
 
+// initial parameter, threshold is rely on the length of history
 PerceptronBranchPredictor::PerceptronBranchPredictor(String name, core_id_t core_id, UInt32 size)
     : BranchPredictor(name, core_id),
     m_history_length(62),
@@ -16,8 +17,10 @@ PerceptronBranchPredictor::PerceptronBranchPredictor(String name, core_id_t core
     m_threshold(static_cast<int>(1.93 * m_history_length + 14))
 
 {
+    // create perceptron table
     m_perceptrons = std::vector<Perceptron>(m_perceptron_table_size);
 
+    // init weights and history as 0.
     for ( int i = 0; i < m_perceptrons.size(); i++ ) {
         m_perceptrons[i].weights = std::vector<int>(m_history_length);
         initWeights((m_perceptrons[i]));
@@ -65,11 +68,13 @@ PerceptronBranchPredictor::Perceptron& PerceptronBranchPredictor::select(int ind
     return m_perceptrons[index];
 }
 
+// update history by erase the beginning history and pad it with last history
 void PerceptronBranchPredictor::updateHistory(bool actual){
     m_history.erase(m_history.begin());
     m_history.push_back(actual ? 1 : -1);
 }
 
+// ignore the last 4 bits, and mod by perceptron table size
 int PerceptronBranchPredictor::hash(IntPtr key) {
     return ((key >> 4) % m_perceptron_table_size);
 }
@@ -78,8 +83,10 @@ bool PerceptronBranchPredictor::predict(IntPtr raw, IntPtr target)
 {
     int index = hash(raw);
     Perceptron &p = select(index);
+    // compute the output of perceptron
     m_last_out = outcome(p);
      
+    // if perceptron output > 0, return true(taken), otherwise return false(not taken)
     return m_last_out > 0 ? true : false;
 }
 
@@ -101,6 +108,7 @@ void PerceptronBranchPredictor::update(bool predicted, bool actual, IntPtr ip, I
     Perceptron &p = select(index);
     int actual_i =  actual ? 1 : -1;
 
+    // if perceptron give a wrong prediction or it has low confidence, train it
     if ( predicted != actual || abs(m_last_out) <= m_threshold ) {
         train(p, actual_i);
     }
@@ -109,6 +117,7 @@ void PerceptronBranchPredictor::update(bool predicted, bool actual, IntPtr ip, I
     //printHistory(m_perceptrons[index].history);
 }
 
+// output of perceptron is computed by the affine equation
 int PerceptronBranchPredictor::outcome(Perceptron &p){
     int y = p.w0;
     for( int i = 0; i < p.weights.size(); i++) {
@@ -129,6 +138,7 @@ int PerceptronBranchPredictor::cut(int y) {
     return out;
 }
 
+// train perceptron follow the paper said
 void PerceptronBranchPredictor::train( Perceptron &p, int t) {
     p.w0 = p.w0 + t;
     for ( int i = 0; i < p.weights.size(); i++ ) {
